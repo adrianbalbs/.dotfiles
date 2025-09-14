2e84b01d14d1fd5dca686362441f521a2a0cce0c
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2025-09-14 14:27:10 +1000

    init mac branch

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,8 @@ if status is-interactive
     set GOPATH "$HOME/go"
     fish_add_path "$GOPATH/bin"
 
+    fish_add_path "/opt/homebrew/bin/"
+
     fzf --fish | source
     zoxide init --cmd cd fish | source
     direnv hook fish | source
