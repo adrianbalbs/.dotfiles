38d0fca558605935726b7a7adef2f83b6ebbf76a
adrianbalbs <adrianbalbs@hotmail.com>
2025-09-27 11:19:25 +1000

    begin migration from bare repo to chezmoi

---
+++ private_dot_config/fish/config.fish
@@ -0,0 +1,15 @@
+set fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -x VOLTA_HOME "$HOME/.volta"
+    fish_add_path "$VOLTA_HOME/bin"
+
+    set GOPATH "$HOME/go"
+    fish_add_path "$GOPATH/bin"
+
+    fzf --fish | source
+    zoxide init --cmd cd fish | source
+    direnv hook fish | source
+    starship init fish | source
+end
