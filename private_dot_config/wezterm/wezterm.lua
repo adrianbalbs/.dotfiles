2c82b4bfb0f807d32a71ac608db3515c641e8208
Adrian Balbalosa <adrianbalbs@pm.me>
2025-09-29 19:42:28 +1000

    make wezterm use everforest

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -15,7 +15,8 @@ end
 
 -- For example, changing the color scheme:
 -- config.colors = theme.colors()
-config.color_scheme = "Tokyo Night Moon"
+config.color_scheme_dirs = { "~/.config/wezterm/colors" }
+config.color_scheme = "Everforest Dark (Medium)"
 config.default_prog = { "/usr/bin/fish", "-l" }
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = true
@@ -23,7 +24,7 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.font = wezterm.font("FiraCode Nerd Font Mono")
-config.font_size = 14
+config.font_size = 12
 -- config.window_background_opacity = 0.85
 -- config.kde_window_background_blur = true
 config.max_fps = 240
