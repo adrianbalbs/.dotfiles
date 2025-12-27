002fb8dbd214681d556f05d555b9fd2cdfb14fc3
Adrian Balbalosa <adrianbalbs@pm.me>
2025-12-28 10:56:15 +1100

    update nvim deps and add refactoring plugin

--- private_dot_config/nvim/lua/config/lsp.lua
+++ private_dot_config/nvim/lua/config/lsp.lua
@@ -38,19 +38,6 @@ vim.lsp.config("*", {
     root_markers = { ".git" },
 })
 
--- For some reason the clangd settings in the lsp folder are not being merged, so need to change here
-vim.lsp.config.clangd = {
-    cmd = {
-        "clangd",
-        "--background-index",
-        "--clang-tidy",
-        "--completion-style=detailed",
-        "--header-insertion=iwyu",
-    },
-    root_markers = { "compile_commands.json", "compile_flags.txt", ".clang-tidy", ".clang-format" },
-    filetypes = { "c", "cpp" },
-}
-
 vim.diagnostic.config { virtual_lines = true }
 
 vim.lsp.enable {
