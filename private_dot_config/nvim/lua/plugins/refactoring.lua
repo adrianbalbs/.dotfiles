c65fd3ecf9612f69ebf70e69e320bb05165d5ced
Adrian Balbalosa <adrianb@langdale.com.au>
2026-04-28 09:10:37 +1000

    add dep bump, add async.nvim dep for refactoring.nvim

--- private_dot_config/nvim/lua/plugins/refactoring.lua
+++ private_dot_config/nvim/lua/plugins/refactoring.lua
@@ -1,9 +1,7 @@
 return {
     "ThePrimeagen/refactoring.nvim",
     dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
+        "lewis6991/async.nvim",
     },
     lazy = false,
-    opts = {},
 }
